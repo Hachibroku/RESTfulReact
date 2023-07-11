91720731d415d2f9e4e8a1217d61ddc980149d96
Murphey Osmundson <mosmundson88@gmail.com>
2023-07-11 18:16:27 -0400

    added columns

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -22,6 +22,7 @@ function createCard(name, description, pictureUrl) {
       } else {
         const data = await response.json();
 
+        let i = 0
         for (let conference of data.conferences) {
           const detailUrl = `http://localhost:8000${conference.href}`;
           const detailResponse = await fetch(detailUrl);
@@ -31,8 +32,9 @@ function createCard(name, description, pictureUrl) {
             const description = details.conference.description;
             const pictureUrl = details.conference.location.picture_url;
             const html = createCard(name, description, pictureUrl);
-            const column = document.querySelector('.col');
+            const column = document.querySelector(`#col_${i%3}`);
             column.innerHTML += html;
+            i++
           }
         }
 
